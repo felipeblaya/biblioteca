9d85ef8ed3de65c6b0f600022f0017f67c7f862b
felipeblaya <felipeblaya3e@gmail.com>
2015-02-27 10:50:09 -0300

    Criação dos Datas

---
+++ BibliotecaClassLibrary/Data/AutorData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using BibliotecaClassLibrary.Entity;
+
+namespace BibliotecaClassLibrary.Data
+{
+    internal class AutorData:DBConnection
+    {
+        internal AutorData(string strConn) : base(strConn){ }
+
+        internal void Create(Autor e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "insert into Categoria values (@nome)";
+        }
+    }
+}
